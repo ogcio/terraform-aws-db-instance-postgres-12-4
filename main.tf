28107be0ed25be21e2d1f5916e0aac4f9b78e831
Maris Erts <33098339+maris-erts@users.noreply.github.com>
2021-01-27 08:16:36 +0000

    main.tf

---
+++ main.tf
@@ -0,0 +1,88 @@
+variable "db_name"                      {  }
+variable "db_username"                  {  }
+variable "db_port"                      { default = 5432 }
+variable "db_engine_version"            { default = "12.4" }
+variable "db_password"                  {  }
+variable "db_allocated_storage"         { default = 20 }
+variable "db_instance_class"            {  }
+variable "db_backup_retention_period"   { default = 30 }
+variable "db_publicly_accessible"       { default = true }
+variable "name"                         { default = "Postgres 12.4 database" }
+variable "subnets"                      { default = [] }
+variable "security_group_ids"           { default = [] }
+
+
+resource "aws_db_subnet_group" "default" {
+  name          = "${var.name}-db-instance-security-group"
+  subnet_ids    = var.subnets
+}
+
+
+resource "aws_db_parameter_group" "default" {
+  name   = "${var.name}-parameter-group"
+  family = "postgres12"
+
+  parameter {
+    name  = "lc_messages"
+    value = "en_IE.UTF-8"
+  }
+
+  parameter {
+    name  = "lc_monetary"
+    value = "en_IE.UTF-8"
+  }
+
+  parameter {
+    name  = "lc_numeric"
+    value = "en_IE.UTF-8"
+  }
+
+  parameter {
+    name  = "lc_time"
+    value = "en_IE.UTF-8"
+  }
+
+  parameter {
+    apply_method = "pending-reboot"
+    name         = "shared_buffers"
+    value        = "{DBInstanceClassMemory/32768}"
+  }
+}
+
+
+resource "aws_db_instance" "default" {
+  allocated_storage           = var.db_allocated_storage
+  allow_major_version_upgrade = false
+  apply_immediately           = true
+  auto_minor_version_upgrade  = false
+  backup_retention_period     = var.db_backup_retention_period
+  backup_window               = "01:00-03:00"
+  db_subnet_group_name        = aws_db_subnet_group.default.name
+  engine                      = "postgres"
+  engine_version              = var.db_engine_version
+  identifier                  = "${var.name}-postgres"
+  instance_class              = var.db_instance_class
+  maintenance_window          = "Mon:03:00-Mon:05:00"
+  monitoring_interval         = "0"
+  multi_az                    = true
+  name                        = var.db_name
+  parameter_group_name        = aws_db_parameter_group.default.id
+  password                    = var.db_password
+  publicly_accessible         = var.db_publicly_accessible
+  skip_final_snapshot         = true
+  storage_type                = "gp2"
+  username                    = var.db_username
+  vpc_security_group_ids      = var.security_group_ids
+
+  tags = {
+    Name = "${var.name}-${var.db_instance_class}-postgres-12.4"
+  }
+}
+
+output "address"      { value = aws_db_instance.default.address }
+output "arn"          { value = aws_db_instance.default.arn }
+output "id"           { value = aws_db_instance.default.id }
+output "db_name"      { value = aws_db_instance.default.name }
+output "db_port"      { value = aws_db_instance.default.port }
+output "db_username"  { value = aws_db_instance.default.username }
+output "resource_id"  { value = aws_db_instance.default.address }
