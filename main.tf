40314298e931b88bafdc0d7f118dd50ec2dbfbf7
Gerry Mcbride <gerry.mcbride@trurisk.io>
2022-04-27 13:14:02 +0100

    Update db version

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ variable "backup_retention_period"      { default = 30 }
 variable "db_name"                      {  }
 variable "db_username"                  {  }
 variable "db_port"                      { default = 5432 }
-variable "db_engine_version"            { default = "12.4" }
+variable "db_engine_version"            { default = "12.8" }
 variable "db_password"                  {  }
 variable "db_allocated_storage"         { default = 20 }
 variable "db_instance_class"            { default = "db.t2.micro" }
